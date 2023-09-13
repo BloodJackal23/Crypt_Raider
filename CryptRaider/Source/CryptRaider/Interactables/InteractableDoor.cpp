d099b1c1285bc929fac7872916c346e0eb1549b7
BloodJackal23 <31773884+BloodJackal23@users.noreply.github.com>
2023-09-14 01:03:05 +0300

    Added InteractableDoor class

---
+++ CryptRaider/Source/CryptRaider/Interactables/InteractableDoor.cpp
@@ -0,0 +1,20 @@
+#include "InteractableDoor.h"
+
+// Sets default values
+AInteractableDoor::AInteractableDoor()
+{
+	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AInteractableDoor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AInteractableDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
