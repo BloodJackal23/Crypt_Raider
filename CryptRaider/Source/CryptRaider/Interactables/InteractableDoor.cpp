e2e3c1108505a51fac383138791f14c29a169b19
BloodJackal23 <31773884+BloodJackal23@users.noreply.github.com>
2023-09-14 22:56:57 +0300

    Made a door say something

--- CryptRaider/Source/CryptRaider/Interactables/InteractableDoor.cpp
+++ CryptRaider/Source/CryptRaider/Interactables/InteractableDoor.cpp
@@ -1,4 +1,6 @@
 #include "InteractableDoor.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/Character.h"
 
 // Sets default values
 AInteractableDoor::AInteractableDoor()
@@ -11,10 +13,18 @@ AInteractableDoor::AInteractableDoor()
 void AInteractableDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	WorldPtr = GetWorld();
 }
 
 // Called every frame
 void AInteractableDoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	DrawDebugBox(WorldPtr, GetTransform().GetLocation(), DetectionExtents, FColor::Cyan, false, -1.0f, 0U, 20.0f);
+	TArray<FOverlapResult> hits;
+	if (WorldPtr->OverlapAnyTestByObjectType(GetTransform().GetLocation(), FQuat::Identity, ECC_Pawn, FCollisionShape::MakeBox(DetectionExtents * 0.5f))) 
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2, FColor::Green, TEXT("Camera next to door!"));
+	}
+	
 }
--- CryptRaider/Source/CryptRaider/Interactables/InteractableDoor.h
+++ CryptRaider/Source/CryptRaider/Interactables/InteractableDoor.h
@@ -8,7 +8,13 @@ UCLASS()
 class CRYPTRAIDER_API AInteractableDoor : public AActor
 {
 	GENERATED_BODY()
-	
+
+private:
+	UWorld* WorldPtr;
+
+	UPROPERTY(EditAnywhere)
+	FVector DetectionExtents;
+
 public:
 	// Sets default values for this actor's properties
 	AInteractableDoor();
